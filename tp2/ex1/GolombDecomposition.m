57d653d1f5c55b74c5fbbba17ac5d4f5a800f3dc
migpereira <jartcrazy@gmail.com>
2017-06-12 18:43:06 +0100

    Ex1 a)

---
+++ tp2/ex1/GolombDecomposition.m
@@ -0,0 +1,180 @@
+function GolombDecomposition( file , M ,predictor)
+  
+    f = dir(file);
+    size = f.bytes;
+
+    difFile = getDifImage(file,predictor);
+        
+   [~,nrOfSymbols] = histFile(file,difFile);
+   % Retirar os simbolos que ocorrem 0 vezes
+   
+   %get golomb sequence
+   seq = GolombSequence(nrOfSymbols,M);
+   relFreq = tabulate(difFile);
+   
+   [values,order] = sort(relFreq(:,3),'descend');
+   sortedRelFreq = relFreq(order,:);
+   
+   fid = fopen( 'results.txt', 'wt' );
+   nbits = 0;
+   for i = 1 : length(difFile)
+       x = difFile(i,1);
+       index = sortedRelFreq(:,1) == x;
+       cell = seq(index); 
+       if(isempty(cell))
+        disp('somethin wrong')
+       end
+       golombCode = cell{1,1};
+       word = num2str(golombCode);
+       word = word(find(~isspace(word)));
+       fprintf(fid,'%s \n',word);
+       nbits = nbits + length(word);
+   end
+   
+   fclose(fid);
+   total_bytes = nbits / 8;
+   fprintf('bytes = %10d \n', total_bytes);
+   fprintf('compression ratio = %.2f %%\n',(total_bytes / size)*100);
+   
+end
+
+
+function [resimage] = getDifImage(file,predictor)
+    % Ler a imagem a partir do ficheiro.
+    fin=fopen(file,'r');
+    
+    I=fread(fin); 
+    image=reshape(I,[256,256]);
+    [M,N] = size(image);
+    
+    filesize = M * N;
+    difImage = zeros(filesize,1);
+    count = 1;
+    for i = 1 : M
+        for j = 1 : N    
+            difImage(count) = 250+(image(j,i) - getPredition(image,predictor,j,i));
+            count = count + 1;
+        end
+    end
+    resimage = difImage;
+end
+
+function [dif] = getPredition(image,predictor,line,col)
+    switch predictor
+        case 0
+            dif = image(line,col);
+        case 1
+            dif = predictor1(image,line,col); 
+        case 2
+            dif = predictor2(image,line,col); 
+        case 3
+            dif = predictor3(image,line,col); 
+        case 4
+            dif = predictor4(image,line,col);  
+        case 5
+            dif = predictor5(image,line,col);  
+        case 6
+            dif = predictor6(image,line,col);  
+        case 7
+            dif = predictor7(image,line,col); 
+        case 8
+            dif = predictor8(image,line,col); 
+        otherwise
+            return;
+    end
+end
+
+function [out] = predictor1(image,line,col)
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        A = image(line,col-1);
+        out = A; 
+    end
+end
+
+function [out] = predictor2(image,line,col)
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        B = image(line-1,col);
+        out = B; 
+    end
+end
+
+function [out] = predictor3(image,line,col)
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        C = image(line-1,col-1);
+        out = C; 
+    end
+end
+
+function [out] = predictor4(image,line,col)
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        A = image(line,col-1);
+        B = image(line-1,col);
+        C = image(line-1,col-1);
+        out = A + B - C; 
+    end
+end
+
+function [out] = predictor5(image,line,col)
+    A = image(line,col-1);
+    B = image(line-1,col);
+    C = image(line-1,col-1);
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        A = image(line,col-1);
+        B = image(line-1,col);
+        C = image(line-1,col-1);
+        out = A + ((B - C) / 2); 
+    end
+end
+
+function [out] = predictor6(image,line,col)
+
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        A = image(line,col-1);
+        B = image(line-1,col);
+        C = image(line-1,col-1);
+        out = B + ((A - C) / 2); 
+    end
+end
+
+function [out] = predictor7(image,line,col)
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        A = image(line,col-1);
+        B = image(line-1,col);
+        out = (A + B) / 2; 
+    end
+end
+
+function [out] = predictor8(image,line,col)
+    if(line == 1 || col == 1)
+        out = image(line,col);
+    else
+        A = image(line,col-1);
+        B = image(line-1,col);
+        C = image(line-1,col-1);
+        if(C >= max(A,B)) 
+            out = min(A,B);
+        else
+            if(C <= min(A,B)) 
+                out = max(A,B);
+            else
+                out = A + B - C; 
+            end
+        end
+    end
+end
+
+
